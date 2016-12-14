6aacedd703fe163611269cb10e2881a235e29d59
Lauren Gibbs <lauren@chartiq.com>
2016-12-14 17:10:00 -0500

    added implementing your own quotefeed examples

--- app/chart_service/chart.service.js
+++ app/chart_service/chart.service.js
@@ -7,6 +7,12 @@ System.register([], function(exports_1, context_1) {
         execute: function() {
             ChartService = (function () {
                 function ChartService() {
+                    // To implement your own quotefeed and see other methods of data loading, check out our tutorial: http://documentation.chartiq.com/tutorial-Data%20Loading.html
+                    CIQ.QuoteFeed.MyFeed = function (url) {
+                        this.url = url;
+                    };
+                    // Inherit from the base feed
+                    CIQ.QuoteFeed.MyFeed.ciqInheritsFrom(CIQ.QuoteFeed);
                 }
                 ChartService.prototype.attachQuoteFeed = function (chart) {
                     chart.attachQuoteFeed(new CIQ.QuoteFeed.Demo(), { refreshInterval: 1 });
