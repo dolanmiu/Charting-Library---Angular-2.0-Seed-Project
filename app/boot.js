a2bacadf25d5ae7f5c2c0987b9dbb52dc0c6a855
Lauren Gibbs <lauren@chartiq.com>
2017-01-31 11:39:05 -0500

    reworked to use requirejs

--- app/boot.js
+++ app/boot.js
@@ -11,7 +11,23 @@ System.register(['angular2/platform/browser', './app.component'], function(expor
                 app_component_1 = app_component_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(app_component_1.AppComponent);
+            // Configure requirejs. Otherwise the baseUrl would default to this directory
+            // TODO change baseUrl to match where your ChartIQ js folder is located. This is how require.js knows where to start looking.
+            require.config({
+                baseUrl: 'chartiq_library/js/',
+            });
+            // Define the AMD module for the library
+            // ['chartiq'] refers to chartiq.js in the ChartIQ library. Requiring this file kicks off require.js and grabs everything that is needed from the library.
+            define('chartIQ', ['chartiq'], function (chartiq) {
+                for (var key in chartiq) {
+                    window[key] = chartiq[key];
+                }
+                return chartiq;
+            });
+            //Bootstrap the app after the required library is loaded
+            require(['chartIQ'], function () {
+                browser_1.bootstrap(app_component_1.AppComponent);
+            });
         }
     }
 });
--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,25 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
 
-bootstrap(AppComponent);
+declare let require: any;
+declare let define: any;
+
+// Configure requirejs. Otherwise the baseUrl would default to this directory
+// TODO change baseUrl to match where your ChartIQ js folder is located. This is how require.js knows where to start looking.
+require.config({
+	baseUrl: 'chartiq_library/js/',
+});
+
+// Define the AMD module for the library
+// ['chartiq'] refers to chartiq.js in the ChartIQ library. Requiring this file kicks off require.js and grabs everything that is needed from the library.
+define('chartIQ', ['chartiq'], function (chartiq) {
+	for (var key in chartiq) {
+		window[key] = chartiq[key];
+	}
+	return chartiq;
+});
+
+//Bootstrap the app after the required library is loaded
+require(['chartIQ'], function() {
+	bootstrap(AppComponent);
+});
