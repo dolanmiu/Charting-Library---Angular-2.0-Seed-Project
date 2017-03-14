e6bea24770a265ed5001f29758d328ca569bae0f
Jay Thomas <jay@gfax.ch>
2017-03-13 21:24:53 -0400

    Add ngOnDestroy cleanup example

--- src/app/chart_component/chart.component.ts
+++ src/app/chart_component/chart.component.ts
@@ -5,39 +5,46 @@ declare var CIQ: any;
 declare var $$$: any;
 
 @Component({
-    selector: 'chart',
-    styleUrls: ['../css/CIQ_Seed.css'],
-    templateUrl: './chart.component.html',
-    providers: [ChartService]
+  selector: 'chart',
+  styleUrls: ['../css/CIQ_Seed.css'],
+  templateUrl: './chart.component.html',
+  providers: [ChartService]
 })
 
 export class ChartComponent implements OnInit {
-    ciq: any;
-    sampleData: any[];
-	  chartSeries:any[];
+  ciq: any;
+  sampleData: any[];
+  chartSeries:any[];
 
-    constructor(private chartService: ChartService) {
-        this.chartSeries=[];
-    };
+  constructor(private chartService: ChartService) {
+    this.chartSeries=[];
+  };
 
-    ngOnInit() {
-        this.ciq = new CIQ.ChartEngine({ container: $$$("#chartContainer")});
-        this.ciq.setPeriodicityV2(1, 5);
-        this.chartService.attachQuoteFeed(this.ciq);
-        this.ciq.newChart("IBM");
+  ngOnInit() {
+    this.ciq = new CIQ.ChartEngine({ container: $$$("#chartContainer")});
+    this.ciq.setPeriodicityV2(1, 5);
+    this.chartService.attachQuoteFeed(this.ciq);
+    this.ciq.newChart("IBM");
+  }
+
+  // https://angular.io/docs/ts/latest/api/core/index/OnDestroy-class.html
+  ngOnDestroy() {
+    // This will remove the quoteDriver, styles and
+    // eventListeners for this ChartEngine instance.
+    this.ciq.destroy();
+  }
+
+  getLayout() {
+    return this.ciq.layout;
+  }
+
+  removeSeries(series){
+    var index = this.chartSeries.indexOf(series, 0);
+    if (index > -1) {
+      this.chartSeries.splice(index, 1);
     }
-
-    getLayout() {
-    	return this.ciq.layout;
-    }
-
-    removeSeries(series){
-	    var index = this.chartSeries.indexOf(series, 0);
-	    if (index > -1) {
-		    this.chartSeries.splice(index, 1);
-	    }
-	    this.ciq.removeSeries(series.display, this.ciq.ciq);
-    }
+    this.ciq.removeSeries(series.display, this.ciq.ciq);
+  }
 
   set(multiplier, span){
     var params={
